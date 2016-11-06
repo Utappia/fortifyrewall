97f24f61edda6fe5cc8e99b9d11fd9b22f048d1f
cerebrux <cerebrux.gr@gmail.com>
2016-11-06 22:55:59 +0200

    Win/SMB: change to drop from reject

--- fortifyrewall.sh
+++ fortifyrewall.sh
@@ -139,9 +139,9 @@ sleep 2
 echo ""
 echo "Disable smb/windows sharing packets because they generate too much logging..."
 iptables -A INPUT -p tcp --dport 137:139 -j LOG --log-prefix "SMB/Windows service Scan : "
-iptables -A INPUT -p tcp --dport 137:139 -j REJECT
+iptables -A INPUT -p tcp --dport 137:139 -j DROP
 iptables -A INPUT -p udp --dport 137:139 -j LOG --log-prefix "SMB/Windows service Scan : "
-iptables -A INPUT -p udp --dport 137:139 -j REJECT
+iptables -A INPUT -p udp --dport 137:139 -j DROP
 sleep 2
 echo ""
 echo "Creating a IP whitelist from which, all connections will be accepted..."
